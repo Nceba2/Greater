f69a1113b5393cadaeff85fb7070964f569a3eed
Nceba2 <ncebasobikwa1@hotmail.com>
2019-04-27 03:06:14 +0200

    This project is just a simple servlet. my first servlet. tests included. nothing complicated or complex about this project for version 1

--- src/main/java/PathController.java
+++ src/main/java/PathController.java
@@ -1,7 +1,17 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 
+@Controller
 public class PathController {
 
+    @GetMapping("/")
+    public String index(Map<String, Object> Model){
+        return "Second response";
+
+    }
 
 
 }
